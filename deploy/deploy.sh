8846bd3f35527c5c7809f687169887419ee63389
LSapee <dlworhks94@gmail.com>
2024-05-10 16:53:45 +0900

    배포관련 수정

---
+++ deploy/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 저장소로부터 최신 코드를 가져오거나 업데이트
+git pull origin main
+
+# Node.js 의존성 설치
+npm install
+
+# prisma
+npx prisma generate
+
+# PM2를 사용하여 애플리케이션 재시작
+pm2 start ../find-job-craling-only/node_modules/.bin/ts-node --name "find-job" -- index.ts
+
+
