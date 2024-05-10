c9370003106728db24d05875920238d97d5b3872
LSapee <dlworhks94@gmail.com>
2024-05-10 18:10:08 +0900

    일단 테스트

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-cd ..
+cd /find-job-craling-only
 # 저장소로부터 최신 코드를 가져오거나 업데이트
 git pull origin main
-
 # Node.js 의존성 설치
 npm install
-
 # prisma
 npx prisma generate
-
 # PM2를 사용하여 애플리케이션 재시작
-pm2 start ../find-job-craling-only/node_modules/.bin/ts-node --name "find-job" -- index.ts
+pm2 start node_modules/.bin/ts-node --name "find-job" -- index.ts
 
 
