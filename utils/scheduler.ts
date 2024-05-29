583741a3cb74b49fe2f321737e91168a8182ea51
LSapee <dlworhks94@gmail.com>
2024-05-29 13:40:26 +0900

    EC2 이슈로 직접 크롤링 시간 단축을 위한 병렬처리?로 변경

--- utils/scheduler.ts
+++ utils/scheduler.ts
@@ -6,18 +6,24 @@ const {ec2Stop} = require("../aws/awsEc2Off");
 // 분 시 일 월 요일
 export const crawlingScheduler = async ()=>{
     let cnt = 0;
-    cron.schedule(("10 00 * * *"), async () =>{
-        // await postDel();
+    const promises:any[] =[];
+    cron.schedule(("37 13 * * *"), async () =>{
+        // 상시채용 ,채용시 완료, 기간 지난 공고 삭제
+        await postDel();
         const keywords : string[] = await findKeywords();
         console.log(keywords)
         for(const item of keywords){
-            await postDel();
-            const resultJok = await jobKCrawler(item);
-            const resultSaramIn = await saramInCrawler(item);
-            console.log(`크롤링 resultJob ${item}:`, resultJok );
-            console.log(`크롤링 resultSaramIn ${item}:`, resultSaramIn );
+            // 기존코드
+            // const resultJok = await jobKCrawler(item);
+            // const resultSaramIn = await saramInCrawler(item);
+            // console.log(`크롤링 resultJob ${item}:`, resultJok );
+            // console.log(`크롤링 resultSaramIn ${item}:`, resultSaramIn );
+            // 병렬처리를 위한 변경 코드
+            promises.push(jobKCrawler(item));
+            promises.push(saramInCrawler(item));
             cnt++;
         }
-        if(cnt==keywords.length) await ec2Stop();
+        await Promise.all(promises);
+        // if(cnt==keywords.length) await ec2Stop();
     })
 }
