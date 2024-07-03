1f3f5ba1be2b9ce835f7f0aaffbbf0832f082853
LSapee <dlworhks94@gmail.com>
2024-07-03 15:51:00 +0900

    포트 변경

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 require('dotenv').config();
 import express, { Request, Response } from 'express';
 const app = express();
-const port = 3000;
+const port = 3002;
 const {crawlingScheduler} = require('./utils/scheduler');
 
 //json 데이터 파싱
