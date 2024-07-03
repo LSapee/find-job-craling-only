eddae721aec4e5285efcff63cde18e08cd96f194
LSapee <dlworhks94@gmail.com>
2024-07-03 15:53:08 +0900

    메모리 부족할 가능성 및 동시에 같은 키워드 충돌로 인한 에러 발생에 대한 수정

--- utils/scheduler.ts
+++ utils/scheduler.ts
@@ -5,25 +5,30 @@ const {ec2Stop} = require("../aws/awsEc2Off");
 
 // 분 시 일 월 요일
 export const crawlingScheduler = async ()=>{
-    let cnt = 0;
     const promises:any[] =[];
-    cron.schedule(("37 13 * * *"), async () =>{
+    const maxSize = 10;
+    cron.schedule(("35 15 * * *"), async () =>{
         // 상시채용 ,채용시 완료, 기간 지난 공고 삭제
         await postDel();
         const keywords : string[] = await findKeywords();
-        console.log(keywords)
         for(const item of keywords){
-            // 기존코드
-            // const resultJok = await jobKCrawler(item);
-            // const resultSaramIn = await saramInCrawler(item);
-            // console.log(`크롤링 resultJob ${item}:`, resultJok );
-            // console.log(`크롤링 resultSaramIn ${item}:`, resultSaramIn );
-            // 병렬처리를 위한 변경 코드
             promises.push(jobKCrawler(item));
+            if(promises.length === maxSize){
+                await Promise.race(promises).then((success)=>{
+                    promises.splice(promises.indexOf(success),1);
+                })
+            }
+        }
+        for(const item of keywords){
             promises.push(saramInCrawler(item));
-            cnt++;
+            if(promises.length === maxSize){
+                await Promise.race(promises).then((success)=>{
+                    promises.splice(promises.indexOf(success),1);
+                })
+            }
         }
         await Promise.all(promises);
+        console.log("크롤링 종료");
         // if(cnt==keywords.length) await ec2Stop();
     })
 }
